89dd2594b77f1077d22905de9ea109535cf074f3
Tim Nelson <tbnelson@gmail.com>
2024-04-09 15:46:50 -0400

    add: base in Vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: '/<REPO>/',
+    base: '/reactNYT/',
     build: {
       outDir: 'build',
     },
